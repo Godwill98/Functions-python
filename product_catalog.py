fae139e803a2e1a1dc310c010cdd84e518263044
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-10-03 09:36:00 +0300

    added product_catalog function

---
+++ product_catalog.py
@@ -0,0 +1,14 @@
+
+class product:
+    def __init__(self,name,price,quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+    
+    def total_quantity(self):
+        return self.price * self.quantity
+
+product1 = product("Laptop", 1200, 3)
+product2 = product("Phone", 800, 2)
+
+print(f"THE total qunatitiies of product {product1.name} is {product1.total_quantity()}")
