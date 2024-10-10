d07da43e6b3b2ddc97bd3318394e42688150d684
Godwill Kiplagat <godwillkiprop@gmail.com>
2024-10-10 12:56:08 +0300

    demonstrated singleInheritance in python

--- singleInheritance.py
+++ singleInheritance.py
@@ -1,13 +1,25 @@
-class Animal:
-    def __init__(self, name):
-        self.name = name
-    def MakeSound(self):
-        print("Some sound")
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#     def MakeSound(self):
+#         print("Some sound")
 
-class Dog(Animal):
-    def MakeSound(self):
-        print("Bark")
+# class Dog(Animal):
+#     def MakeSound(self):
+#         print("Bark")
 
-Dog1 = Dog("Simba").MakeSound() # Output: Bark
+# Dog1 = Dog("Simba").MakeSound() # Output: Bark
 
-Dog1.MakeSound() # Output: Bark
+class Shape:
+    def calculate_area(self):
+        pass
+class Rectangle(Shape):
+    def __init__(self,h,w):
+        self.h = h
+        self.w = w
+    def calculate_area(self):
+        return self.h*self.w
+    
+   
+rect1 = Rectangle(5,10)
+print(rect1.calculate_area()) # Output
